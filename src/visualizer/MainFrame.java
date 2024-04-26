bf3fda325f90ebbdc383f2fd07b29ea1ec528783
james <james1.6@hotmail.com>
2024-04-27 00:21:18 +0930

    Add basic structure for Graph Algorithm Visualizer
    The application's skeleton has been set up with ApplicationRunner.java and MainFrame.java under the visualizer package. Also implemented .gitignore to exclude build, output, and test directories from version control.

---
+++ src/visualizer/MainFrame.java
@@ -0,0 +1,12 @@
+package visualizer;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+        setLayout(null);
+    }
+}
