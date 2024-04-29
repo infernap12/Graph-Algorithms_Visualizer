fd443217f49012443ba45b4c0d32eb60309decea
james <james1.6@hotmail.com>
2024-04-30 00:07:23 +0930

    :white_check_mark: Pass Stage 3
    Add Vertex, Edge and state functionality to visualizer
    (I didn't expect this to work)
    
    This update includes a number of significant alterations to the graph visualizer program. Vertex and Edge elements are introduced, along with their respective functions for their creation, removal, and interaction. Furthermore, the state functionality has been enhanced to allow users to coordinates these actions more effectively.

--- src/visualizer/Graph.java
+++ src/visualizer/Graph.java
@@ -4,12 +4,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Graph extends JPanel {
     private static final int VERTEX_HEIGHT = 50;
     private static final int VERTEX_WIDTH = 50;
 
-    private JPanel[] vertices;
+    ToolState state = ToolState.VERTEX_CREATION;
+    private java.util.List<JPanel> vertices = new ArrayList<>();
+    List<Edge> edges = new ArrayList<Edge>();
+    EdgeManufacturer edgeFabricator = new EdgeManufacturer();
 
     public Graph() {
         super();
@@ -20,7 +26,10 @@ public class Graph extends JPanel {
 
         this.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e) {
+            public void mouseReleased(MouseEvent e) {
+                if (Graph.this.state != ToolState.VERTEX_CREATION) {
+                    return;
+                }
                 String input;
                 while (true) {
                     input = JOptionPane.showInputDialog(null, "Enter Vertex ID", "Vertex", JOptionPane.PLAIN_MESSAGE);
@@ -32,10 +41,8 @@ public class Graph extends JPanel {
 
                         Vertex vertex = new Vertex(input);
                         Graph.this.add(vertex);
-                        vertex.setBounds(e.getX() - (Graph.VERTEX_WIDTH / 2),
-                                e.getY() - (Graph.VERTEX_HEIGHT / 2),
-                                Graph.VERTEX_WIDTH,
-                                Graph.VERTEX_HEIGHT);
+                        vertices.add(vertex);
+                        vertex.setBounds(e.getX() - (Graph.VERTEX_WIDTH / 2), e.getY() - (Graph.VERTEX_HEIGHT / 2), Graph.VERTEX_WIDTH, Graph.VERTEX_HEIGHT);
                         Graph.this.revalidate();
                         Graph.this.repaint();
                         break;
@@ -47,26 +54,43 @@ public class Graph extends JPanel {
         });
     }
 
-    private void createDebugVertices() {
-        vertices = new JPanel[4];
-        for (int i = 0; i < 4; i++) {
-            vertices[i] = new Vertex(String.valueOf(i));
-            this.add(vertices[i]);
-        }
-    }
-
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+        //fuck this, do it in graph
+        for (Edge edge : this.edges) {
+            g.setColor(Color.WHITE);
+            int[] coords = edge.getLineCoords();
+            System.out.println(Arrays.toString(coords));
+            g.drawLine(coords[0], coords[1], coords[2], coords[3]);
+        }
 //        drawDebugVertices();
     }
 
-    private void drawDebugVertices() {
-        int width = Graph.VERTEX_WIDTH;
-        int height = Graph.VERTEX_HEIGHT;
-        vertices[0].setBounds(0, 0, width, height);
-        vertices[1].setBounds(this.getWidth() - width, 0, width, height);
-        vertices[2].setBounds(0, this.getHeight() - height, width, height);
-        vertices[3].setBounds(this.getWidth() - width, this.getHeight() - height, width, height);
+//    private void createDebugVertices() {
+//        vertices = new JPanel[4];
+//        for (int i = 0; i < 4; i++) {
+//            vertices[i] = new Vertex(String.valueOf(i));
+//            this.add(vertices[i]);
+//        }
+//    }
+
+    enum ToolState {
+        NONE("None"), EDGE_CREATION("Add an Edge"), EDGE_DELETION("Remove an Edge"), VERTEX_CREATION("Add a Vertex"), VERTEX_DELETION("Remove a Vertex");
+
+        final String NAME;
+
+        ToolState(String stateName) {
+            this.NAME = stateName;
+        }
     }
+
+//    private void drawDebugVertices() {
+//        int width = Graph.VERTEX_WIDTH;
+//        int height = Graph.VERTEX_HEIGHT;
+//        vertices[0].setBounds(0, 0, width, height);
+//        vertices[1].setBounds(this.getWidth() - width, 0, width, height);
+//        vertices[2].setBounds(0, this.getHeight() - height, width, height);
+//        vertices[3].setBounds(this.getWidth() - width, this.getHeight() - height, width, height);
+//    }
 }
